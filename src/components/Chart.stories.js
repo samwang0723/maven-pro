0f03eba3ecb537f6c5109c4b7783280f425dc02b
Sam Wang <sam.wang.0723@gmail.com>
2023-12-04 16:05:07 +0800

    fix: change to lightweight charts to solve the blurry problem

--- src/components/Chart.stories.js
+++ src/components/Chart.stories.js
@@ -8,34 +8,16 @@ export default {
 
 const Template = (args) => <Chart {...args} />;
 
-function parseDateToTimestamp(dateString) {
-  // Check if the input is valid
-  if (!/^\d{8}$/.test(dateString)) {
-    throw new Error('Invalid date format. Please use "YYYYMMDD".');
-  }
-
-  // Extract year, month, and day from the dateString
-  const year = parseInt(dateString.substring(0, 4), 10);
-  const month = parseInt(dateString.substring(4, 6), 10) - 1; // Month is 0-indexed in JavaScript Date
-  const day = parseInt(dateString.substring(6, 8), 10);
-
-  // Create a new Date object
-  const date = new Date(year, month, day);
-
-  // Return the timestamp
-  return date.getTime();
-}
-
 function parseStockData(stockData) {
   return stockData.map((item) => {
     // Return a new object with only the required fields
     return {
-      timestamp: parseDateToTimestamp(item.date),
+      timestamp: item.date.replace(/(\d{4})(\d{2})(\d{2})/, '$1-$2-$3'),
       open: item.open,
       high: item.high,
       low: item.low,
       close: item.close,
-      volume: item.tradeShares / 1000,
+      volume: Number(item.tradeShares),
       turnover: item.turnover,
     };
   });
@@ -57,17 +39,6 @@ const jsonData = [
     high: 578,
     low: 574,
     diff: -3,
-    average: {
-      ma: {
-        21: 560.58,
-        8: 579.5,
-      },
-      mv: {
-        13: 24916,
-        34: 24897,
-        5: 21561,
-      },
-    },
   },
   {
     id: '488544189982703964',
@@ -84,17 +55,6 @@ const jsonData = [
     high: 578,
     low: 574,
     diff: 1,
-    average: {
-      ma: {
-        21: 558.48,
-        8: 579.13,
-      },
-      mv: {
-        13: 25295,
-        34: 25336,
-        5: 24069,
-      },
-    },
   },
   {
     id: '488399235071344988',
@@ -111,17 +71,6 @@ const jsonData = [
     high: 579,
     low: 574,
     diff: -8,
-    average: {
-      ma: {
-        21: 556.86,
-        8: 578.25,
-      },
-      mv: {
-        13: 26499,
-        34: 25776,
-        5: 26936,
-      },
-    },
   },
   {
     id: '488254422162342236',
@@ -138,17 +87,6 @@ const jsonData = [
     high: 585,
     low: 581,
     diff: 8,
-    average: {
-      ma: {
-        21: 555.29,
-        8: 575.75,
-      },
-      mv: {
-        13: 25974,
-        34: 25544,
-        5: 32899,
-      },
-    },
   },
   {
     id: '488109324510429532',
@@ -165,17 +103,6 @@ const jsonData = [
     high: 579,
     low: 575,
     diff: -3,
-    average: {
-      ma: {
-        21: 553.34,
-        8: 572.25,
-      },
-      mv: {
-        13: 25416,
-        34: 25203,
-        5: 29972,
-      },
-    },
   },
   {
     id: '487674458786496840',
@@ -192,16 +119,6 @@ const jsonData = [
     high: 583,
     low: 579,
     diff: -3,
-    average: {
-      ma: {
-        21: 552.34,
-        8: 569.63,
-      },
-      mv: {
-        13: 24755,
-        5: 34653,
-      },
-    },
   },
   {
     id: '487529503992578376',
@@ -218,16 +135,6 @@ const jsonData = [
     high: 583,
     low: 578,
     diff: 2,
-    average: {
-      ma: {
-        21: 550.72,
-        8: 566.5,
-      },
-      mv: {
-        13: 25035,
-        5: 32758,
-      },
-    },
   },
   {
     id: '487384548661789000',
@@ -244,16 +151,6 @@ const jsonData = [
     high: 583,
     low: 575,
     diff: 9,
-    average: {
-      ma: {
-        21: 548.67,
-        8: 562.38,
-      },
-      mv: {
-        13: 24473,
-        5: 29629,
-      },
-    },
   },
   {
     id: '487239593599435080',
@@ -270,16 +167,6 @@ const jsonData = [
     high: 576,
     low: 571,
     diff: 1,
-    average: {
-      ma: {
-        21: 547.24,
-        8: 558.38,
-      },
-      mv: {
-        13: 21660,
-        5: 22313,
-      },
-    },
   },
   {
     id: '487094638620967240',
@@ -296,16 +183,6 @@ const jsonData = [
     high: 580,
     low: 571,
     diff: 14,
-    average: {
-      ma: {
-        21: 545.96,
-        8: 555.25,
-      },
-      mv: {
-        13: 22166,
-        5: 20792,
-      },
-    },
   },
   {
     id: '486659773383573832',
@@ -322,16 +199,6 @@ const jsonData = [
     high: 557,
     low: 554,
     diff: 0,
-    average: {
-      ma: {
-        21: 545.1,
-        8: 549.88,
-      },
-      mv: {
-        13: 19922,
-        5: 17705,
-      },
-    },
   },
   {
     id: '486514818119893319',
@@ -348,16 +215,6 @@ const jsonData = [
     high: 557,
     low: 551,
     diff: 1,
-    average: {
-      ma: {
-        21: 544.77,
-        8: 546.38,
-      },
-      mv: {
-        13: 20074,
-        5: 18217,
-      },
-    },
   },
   {
     id: '486369862470336839',
@@ -374,16 +231,6 @@ const jsonData = [
     high: 557,
     low: 550,
     diff: 1,
-    average: {
-      ma: {
-        21: 544.15,
-        8: 543.25,
-      },
-      mv: {
-        13: 20813,
-        5: 21557,
-      },
-    },
   },
   {
     id: '486224907626086727',
@@ -400,16 +247,6 @@ const jsonData = [
     high: 555,
     low: 550,
     diff: 5,
-    average: {
-      ma: {
-        21: 543,
-        8: 540.38,
-      },
-      mv: {
-        13: 23222,
-        5: 21143,
-      },
-    },
   },
   {
     id: '486079952530178375',
@@ -426,16 +263,6 @@ const jsonData = [
     high: 556,
     low: 550,
     diff: 1,
-    average: {
-      ma: {
-        21: 541.72,
-        8: 537.38,
-      },
-      mv: {
-        13: 23842,
-        5: 23392,
-      },
-    },
   },
   {
     id: '485645086906908999',
@@ -452,16 +279,6 @@ const jsonData = [
     high: 549,
     low: 545,
     diff: 2,
-    average: {
-      ma: {
-        21: 540.29,
-        8: 536.63,
-      },
-      mv: {
-        13: 23564,
-        5: 21670,
-      },
-    },
   },
   {
     id: '485500131592896839',
@@ -478,16 +295,6 @@ const jsonData = [
     high: 547,
     low: 535,
     diff: 19,
-    average: {
-      ma: {
-        21: 539.34,
-        8: 536,
-      },
-      mv: {
-        13: 23801,
-        5: 21682,
-      },
-    },
   },
   {
     id: '485355176681537863',
@@ -504,16 +311,6 @@ const jsonData = [
     high: 533,
     low: 527,
     diff: -1,
-    average: {
-      ma: {
-        21: 538.67,
-        8: 535.63,
-      },
-      mv: {
-        13: 23036,
-        5: 21522,
-      },
-    },
   },
   {
     id: '485210221652738375',
@@ -530,15 +327,6 @@ const jsonData = [
     high: 535,
     low: 527,
     diff: -3,
-    average: {
-      ma: {
-        8: 539.13,
-      },
-      mv: {
-        13: 24442,
-        5: 22088,
-      },
-    },
   },
   {
     id: '485065266691047751',
@@ -555,15 +343,6 @@ const jsonData = [
     high: 534,
     low: 528,
     diff: -1,
-    average: {
-      ma: {
-        8: 541.25,
-      },
-      mv: {
-        13: 25126,
-        5: 19859,
-      },
-    },
   },
   {
     id: '484630401336213831',
@@ -580,15 +359,6 @@ const jsonData = [
     high: 536,
     low: 532,
     diff: 2,
-    average: {
-      ma: {
-        8: 542.25,
-      },
-      mv: {
-        13: 27150,
-        5: 20113,
-      },
-    },
   },
   {
     id: '484485446223528455',
@@ -605,15 +375,6 @@ const jsonData = [
     high: 535,
     low: 530,
     diff: -13,
-    average: {
-      ma: {
-        8: 544.5,
-      },
-      mv: {
-        13: 27121,
-        5: 26374,
-      },
-    },
   },
   {
     id: '484340491345723911',
@@ -630,15 +391,6 @@ const jsonData = [
     high: 551,
     low: 544,
     diff: 0,
-    average: {
-      ma: {
-        8: 546.25,
-      },
-      mv: {
-        13: 26800,
-        5: 25151,
-      },
-    },
   },
   {
     id: '484195536753132039',
@@ -655,15 +407,6 @@ const jsonData = [
     high: 546,
     low: 540,
     diff: 0,
-    average: {
-      ma: {
-        8: 547.38,
-      },
-      mv: {
-        13: 27802,
-        5: 27173,
-      },
-    },
   },
   {
     id: '484050580835140103',
@@ -680,15 +423,6 @@ const jsonData = [
     high: 553,
     low: 543,
     diff: -12,
-    average: {
-      ma: {
-        8: 548.13,
-      },
-      mv: {
-        13: 27765,
-        5: 27906,
-      },
-    },
   },
   {
     id: '483615856459252014',
@@ -705,15 +439,6 @@ const jsonData = [
     high: 556,
     low: 546,
     diff: 10,
-    average: {
-      ma: {
-        8: 548.13,
-      },
-      mv: {
-        13: 27977,
-        5: 27336,
-      },
-    },
   },
   {
     id: '483470901615001900',
@@ -730,19 +455,16 @@ const jsonData = [
     high: 548,
     low: 540,
     diff: 6,
-    average: {
-      ma: {
-        8: 545.13,
-      },
-      mv: {
-        5: 24315,
-      },
-    },
   },
 ];
 
 const dataArray = parseStockData(jsonData);
-dataArray.sort((a, b) => a.timestamp - b.timestamp);
+dataArray.sort((a, b) => {
+  if (a.timestamp < b.timestamp) {
+    return -1;
+  }
+  return 0;
+});
 
 export const Default = Template.bind({});
 Default.args = {
