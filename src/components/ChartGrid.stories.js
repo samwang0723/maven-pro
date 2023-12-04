ab25ac7798224f9f8a14e1c074460a1186f0450d
Sam Wang <sam.wang.0723@gmail.com>
2023-12-04 18:42:24 +0800

    feat: support real api query

--- src/components/ChartGrid.stories.js
+++ src/components/ChartGrid.stories.js
@@ -1,7 +1,6 @@
 import React from 'react';
 
 import ChartGrid from './ChartGrid';
-import * as ChartStories from './Chart.stories';
 
 export default {
   component: ChartGrid,
@@ -12,11 +11,5 @@ const Template = (args) => <ChartGrid {...args} />;
 
 export const Default = Template.bind({});
 Default.args = {
-  stocks: [
-    { ...ChartStories.Default.args, id: '1' },
-    { ...ChartStories.Default.args, id: '2' },
-    { ...ChartStories.Default.args, id: '3' },
-    { ...ChartStories.Default.args, id: '4' },
-    { ...ChartStories.Default.args, id: '5' },
-  ],
+  bearerToken: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJlMzYwYWYyOS1hYzFhLTQ2OTctYTNhNi0wZDE0YmExNzI3ZjgiLCJhdWQiOiJqYXJ2aXMiLCJpc3MiOiJzYW0ud2FuZy4wNzIzQGdtYWlsLmNvbSIsInN1YiI6IjQ4MzcyNzYyOTE0MTE0Nzk1MSIsImV4cCI6MTcwMjA4MTI3MH0.xl1Fs-gxcSDR9AVgIw9-TiY3ycdgonHuUBnNSIbeP8o',
 };
---
+++ src/components/useStocksWithHistory.jsx
@@ -0,0 +1,67 @@
+import { useState, useEffect } from 'react';
+
+const useStocksWithHistory = (bearerToken, startDate) => {
+  const [stocks, setStocks] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchStocks = async () => {
+      try {
+        const headers = {
+          'Authorization': `Bearer ${bearerToken}`,
+          'Content-Type': 'application/json',
+        };
+        const stockListResponse = await fetch(
+          'http://k3d-k3s-default-server-0.orb.local:8080/v1/pickedstocks',
+          { headers }
+        );
+        if (!stockListResponse.ok) {
+          throw new Error(`HTTP error! status: ${stockListResponse.status}`);
+        }
+        const stockListData = await stockListResponse.json();
+        const stocksWithHistoryPromises = stockListData.entries.map(
+          async (stock) => {
+            const historyPayload = {
+              offset: 0,
+              limit: 60,
+              searchParams: {
+                stockID: stock.stockID,
+                start: startDate,
+              },
+            };
+            const historyResponse = await fetch(
+              `http://k3d-k3s-default-server-0.orb.local:8080/v1/dailycloses`,
+              {
+                method: 'POST',
+                headers: headers,
+                body: JSON.stringify(historyPayload),
+              }
+            );
+            const historyData = await historyResponse.json();
+            return {
+              id: stock.stockID,
+              stockName: stock.stockID,
+              close: stock.close,
+              diff: stock.diff,
+              diffPercent: stock.quoteChange + '%',
+              data: historyData.entries,
+            };
+          }
+        );
+        const stocksWithHistory = await Promise.all(stocksWithHistoryPromises);
+        setStocks(stocksWithHistory);
+      } catch (e) {
+        setError(e.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchStocks();
+  }, [bearerToken, startDate]);
+
+  return { stocks, loading, error };
+};
+
+export default useStocksWithHistory;
