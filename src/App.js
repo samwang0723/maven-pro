778fd6c2b70af35c337e00a989ac4a8f0009dcf3
Sam Wang <sam.wang.0723@gmail.com>
2023-12-05 23:45:00 +0800

    feat: update to use bootstrap and adjust the background

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJlMzYwYWYyOS1hYzFhLTQ2OTctYTNhNi0wZDE0YmExNzI3ZjgiLCJhdWQiOiJqYXJ2aXMiLCJpc3MiOiJzYW0ud2FuZy4wNzIzQGdtYWlsLmNvbSIsInN1YiI6IjQ4MzcyNzYyOTE0MTE0Nzk1MSIsImV4cCI6MTcwMjA4MTI3MH0.xl1Fs-gxcSDR9AVgIw9-TiY3ycdgonHuUBnNSIbeP8o',
   };
   return (
-    <div className="App">
+    <div className="App centered-content bg-dark text-white">
      <ChartGrid {...args} />
     </div>
   );
